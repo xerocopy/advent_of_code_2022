5cc8004c10eda6ff28f5587ca8fe9971f4f22614
jingchao song <jingchaosong.work@hotmail.com>
2022-12-08 12:52:27 +1100

    minor updates

--- day_1.py
+++ day_1.py
@@ -1,18 +1,19 @@
 # Calorie Counting
-cals = []
-tot = 0
-with open('input1.txt', mode = 'r') as f:
-    for line in f.readlines():
-        if line == '\n':
-            print('another elf appeared ....')
-            cals.append(tot)
-            tot = 0
-            print('total cals for last appeared elf: {}'.format(cals[-1]))
-        else:
-            tot += int(line)
+if __name__ == '__main__':
+    cals = []
+    tot = 0
+    with open('input1.txt', mode = 'r') as f:
+        for line in f.readlines():
+            if line == '\n':
+                print('another elf appeared ....')
+                cals.append(tot)
+                tot = 0
+                print('total cals for last appeared elf: {}'.format(cals[-1]))
+            else:
+                tot += int(line)
 
-#print(max(cals))
-sorted_cals = sorted(cals, reverse = True)
-print(sorted_cals[0] + sorted_cals[1] + sorted_cals[2])
+    #print(max(cals))
+    sorted_cals = sorted(cals, reverse = True)
+    print(sorted_cals[0] + sorted_cals[1] + sorted_cals[2])
 
 
