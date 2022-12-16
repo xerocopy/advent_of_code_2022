1353771718a81ea94e99b5acc978523e1a31fcd6
jingchao song <jingchaosong.work@hotmail.com>
2022-12-16 18:04:48 +1100

    update day 6

--- day_6.py
+++ day_6.py
@@ -1,6 +1,32 @@
 #Day 6: Tuning Trouble
 
 
+def openfile():
+    with open('input6.txt', mode = 'r') as f:
+        return f.read()
+
+def findsignal(str, l):
+    for i in range(len(str)-l +1):
+        marker = []
+        for j in range(l):
+            marker.append(str[i + j])
+        print(i, marker)
+        if checkmarker(marker, l):
+            print('Find marker at i = {}.'.format(i))
+            return i
+
+def checkmarker(marker, l):
+    if len(marker) < l:
+        return False
+    elif len(marker) == l:
+        while len(marker) != 0:
+            if marker.pop() in marker:
+                return False
+        return True
+
+
 if __name__ == '__main__':
-    with open('input6.txt', mode ='r') as f:
-        print(f.read())
+    f = openfile()
+    l = int(14)
+    print(findsignal(f, l) + l)
+
