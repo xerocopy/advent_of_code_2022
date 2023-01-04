e59e81359b67c8825a57e3dcdbe1112a07f9b625
xerocopy <jingchaosong.work@hotmail.com>
2023-01-04 23:40:43 +1100

    solving day 11 ... will continue if have time tmr

--- day_11.py
+++ day_11.py
@@ -1,13 +1,25 @@
-# Monkey in the middle
+# Monkey in the Middle
 
 def openfile():
     with open('input11.txt', 'r') as f:
         file = f.readlines()
         return file
 
+class Monkey:
+    def __init__(self, number, starting_items):
+
+    def operation(self):
+        new = old + 8
+    def test(self, ?):
+        if ? % 11 == 0:
+            throw to monkey 5
+        elif ? % 11 != 6:
+            throw to monkey 6
+
 
 def Part1():
-
+    for line in openfile():
+        print(line.strip())
 
 
 def main():
