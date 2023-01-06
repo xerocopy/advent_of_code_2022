12c3a2b2a31fc68552c09bd94dfa154a54b767bb
xerocopy <jingchaosong.work@hotmail.com>
2023-01-06 23:31:39 +1100

    solving day 11

--- day_11.py
+++ day_11.py
@@ -6,33 +6,30 @@ def openfile():
         return file
 
 class Monkey:
-    def __init__(self, name, starting_items, operation, test, if_true, if_false):
+    def __init__(self, name, starting_items, operation, tester, if_true, if_false):
         self.name = name
         self.starting_items = starting_items
-        self.operation = operation
         self.test = test
         self.if_true = if_true
         self.if_false = if_false
+    #
+    # def multiply(self, x):
+    #     return x * self.n
+    #
+    # def square(self, x):
+    #     return x ^^ 2
+    #
+    # def plus(self, x):
+    #     return x + self.n
+    #
+    # def test(self, x):
+    #     if x % test == 0:
+    #         return if_true
+    #     elif x % test != 0:
+    #         return if_false
 
-    operator = operation.splix(' ')[3]
-    n = operation.split(' ')[4]
-
-    if operator == '*' and n == 'old':
-        def operation(x):
-            return x ** 2
-
-    elif operator == '+':
-        def operate(self):
-            return x + int(n)
-
-    def devide(x):
-        if x % test == 0:
-            return if_true
-        elif x % test != 0:
-            return if_false
-
-    def __repr__(self):
-        return name, starting_items, operation, test, if_true, if_false
+    # def __repr__(self):
+    #     return name, starting_items, operator, n, test, if_true, if_false
 
 
 
@@ -40,16 +37,20 @@ def Part1():
     file = openfile()
     monkeys = file.split('\n\n') # monkey is a list
     for monkey in monkeys:
-        print(monkey)
+        #print(monkey)
         list = monkey.split('\n')
-        name = list[0].strip(':')
+        name = list[0].strip(':').replace(' ', '_')
         starting_items = list[1].split(':')[1].split(',')
         operation = list[2].split(':')[1]
-        test = list[3].split(':')[1].split(' ')[-1]
+        # operator = operation.split(' ')[3]
+        # n = operation.split(' ')[4]
+        tester = list[3].split(':')[1].split(' ')[-1]
         if_true = list[4].split(':')[1].split(' ')[-1]
         if_false = list[5].split(':')[1].split(' ')[-1]
-        print(name, starting_items, operation, test, if_true, if_false)
-        Monkey(name, starting_items, operation, test, if_true, if_false)
+
+        print(name, starting_items, operation, tester, if_true, if_false)
+        #Monkey(name, starting_items, operation, tester, if_true, if_false)
+
 
 
     # for i in str:
